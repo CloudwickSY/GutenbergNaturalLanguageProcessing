eb3debd7fcf01b0c41e7b9302ab0fa59b62144f8
Stanislav Yotov <yotov.st@gmail.com>
2016-03-07 11:53:51 +0000

    Pattern

--- src/NaturalLanguageProcessing/WordCount.java
+++ src/NaturalLanguageProcessing/WordCount.java
@@ -4,22 +4,22 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WordCount {
 	public static void main(String[] argv) {
 		String wordToCount = "man", fileName = "pg2600.txt";
 		int count = 0;
 		BufferedReader br;
+		
 		try {
 			br = new BufferedReader(new FileReader(fileName));
 			String line = null;
+			Pattern pattern = Pattern.compile("\b"+wordToCount+"\b");
 			while ((line = br.readLine()) != null) {
-				line.replaceAll("\\^[A-z]", " ");
-				for (String word : line.split(" ")) {
-					if (word.compareToIgnoreCase(wordToCount) == 0) {// word.compareToIgnoreCase(wordToCount)==0
-						count++;
-					}
-				}
+				Matcher matcher = pattern.matcher(line);
+				while(matcher.find()) count++;
 			}
 		} catch (FileNotFoundException e) {
 			System.out.printf("File not found.\n");
