ae6ceac2dd698c2fc324ddfe32b0cbd30fea9f11
Stanislav Yotov <yotov.st@gmail.com>
2016-03-07 11:48:47 +0000

    init commit

---
+++ src/NaturalLanguageProcessing/WordCount.java
@@ -0,0 +1,32 @@
+package NaturalLanguageProcessing;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WordCount {
+	public static void main(String[] argv) {
+		String wordToCount = "man", fileName = "pg2600.txt";
+		int count = 0;
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(fileName));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				line.replaceAll("\\^[A-z]", " ");
+				for (String word : line.split(" ")) {
+					if (word.compareToIgnoreCase(wordToCount) == 0) {// word.compareToIgnoreCase(wordToCount)==0
+						count++;
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.printf("File not found.\n");
+		} catch (IOException e) {
+			System.out.printf("BufferReader Error.\n");
+		} finally {
+			System.out.printf("The occurance of the word %s in the fille was %d.\n", wordToCount, count);
+		}
+	}
+}
