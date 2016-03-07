5e38aa6c109a9a4379957f7c6fe6a772fe980478
Stanislav Yotov <yotov.st@gmail.com>
2016-03-07 21:10:28 +0000

    Sorted the Array of Map.Entry elements containing the word count and saved the top10 to file.

---
+++ src/NaturalLanguageProcessing/NLP.java
@@ -0,0 +1,5 @@
+package NaturalLanguageProcessing;
+
+public class NLP {
+
+}
--- src/NaturalLanguageProcessing/WordCount.java
+++ src/NaturalLanguageProcessing/WordCount.java
@@ -6,26 +6,30 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class WordCount {
 	public static void main(String[] argv) {
-		String wordToCount = "the", fileName = "pg2600.txt";
-		//int count = 0;
-		HashMap<String,Integer> wordHashMap = new HashMap<String,Integer>();
+		String fileName = "pg2600.txt";
+		HashMap<String, Integer> wordHashMap = new HashMap<String, Integer>();
 		BufferedReader br;
 		try {
 			br = new BufferedReader(new FileReader(fileName));
 			String line = null;
 			while ((line = br.readLine()) != null) {
 				line = line.replaceAll("[^A-z]", " ");
-				for (String word : line.split(" ")){
-					word=word.toLowerCase(Locale.ENGLISH);
-					if(wordHashMap.containsKey(word)){
-						wordHashMap.put(word, wordHashMap.get(word)+1);
-					}else{
-						wordHashMap.put(word,1);
+				for (String word : line.split(" ")) {
+					word = word.toLowerCase(Locale.ENGLISH);
+					if(word.isEmpty()){continue;}
+					if (wordHashMap.containsKey(word)) {
+						wordHashMap.put(word, wordHashMap.get(word) + 1);
+					} else {
+						wordHashMap.put(word, 1);
 					}
 				}
 			}
@@ -34,20 +38,49 @@ public class WordCount {
 		} catch (IOException e) {
 			System.out.printf("BufferReader Error.\n");
 		} finally {
-			//System.out.printf("The occurance of the word %s in the fille was %d.\n", wordToCount, count);
+			// System.out.printf("The occurrence of the word %s in the file was
+			// %d.\n", wordToCount, count);
 		}
 		PrintWriter writer = null;
 		try {
 			writer = new PrintWriter("tf.csv", "UTF-8");
 			writer.println("word, frequency");
-			for (String word : wordHashMap.keySet()){
-				writer.printf("%s, %d\n",word, wordHashMap.get(word));
+			for (String word : wordHashMap.keySet()) {
+				writer.printf("%s, %d\n", word, wordHashMap.get(word));
 			}
 		} catch (FileNotFoundException | UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
+		} finally {
 			writer.close();
 		}
+		
+		ArrayList<Map.Entry<String, Integer>> sortedList = entriesSortedByValues(wordHashMap);
+		try {
+			writer = new PrintWriter("top10.csv", "UTF-8");
+			writer.println("word, frequency");
+			Map.Entry<String, Integer> entry;
+			for (int i = 0; i < Math.min(10,sortedList.size()); i++) {
+				entry = sortedList.get(i);
+				writer.printf("%s, %d\n",entry.getKey(),entry.getValue());
+			}
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			writer.close();
+		}
+		
+	}
+
+	static <K, V extends Comparable<? super V>> ArrayList<Map.Entry<K, V>> entriesSortedByValues(Map<K, V> map) {
+		ArrayList<Map.Entry<K, V>> sortedEntries = new ArrayList<Map.Entry<K, V>>(map.entrySet());
+		Collections.sort(sortedEntries, new Comparator<Map.Entry<K, V>>() {
+			@Override
+			public int compare(Map.Entry<K, V> e1, Map.Entry<K, V> e2) {
+				return e2.getValue().compareTo(e1.getValue());
+			}
+		});
+		return sortedEntries;
 	}
 }
